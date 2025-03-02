dbf47f28a634736de48019305c9e5a40975c67d6
Felgy1 <jmeza9799@sdsu.edu>
2025-03-02 01:26:04 -0800

    Pre README

--- SpaceRoute.cpp
+++ SpaceRoute.cpp
@@ -30,23 +30,176 @@ public:
 template <typename T>
 class SpaceRoute {
 private:
+    int length;
     Node<T>* head;
     Node<T>* tail;
 
 public:
-    SpaceRoute();  // Constructor
-    ~SpaceRoute(); // Destructor
+    SpaceRoute() {
+        length = 0;
+        this->head = nullptr;
+        this->tail = nullptr;
+}  // Constructor
+    ~SpaceRoute() {
+        Node<T>* temp = this->head;
+        while (head) {
+            head = head->next;
+            delete temp;
+            temp = head;
+        }
+    }// Destructor
 
-    void addWaypointAtBeginning(T& data);
-    void addWaypointAtEnd(T& data);
-    void addWaypointAtIndex(int index, T& data);
-    void removeWaypointAtBeginning();
-    void removeWaypointAtEnd();
-    void removeWaypointAtIndex(int index);
-    void traverseForward();
-    void traverseBackward();
-    Node<T>* getWaypoint(int index);
-    void setWaypoint(int index, T& data);
+
+    void addWaypointAtBeginning(T& data) {
+        Node<T>* newNode = new Node<T>(data);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    void addWaypointAtEnd(T& data) {
+        Node<T>* newNode = new Node<T>(data);
+        if (length == 0) {
+            tail = newNode;
+            head = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    void addWaypointAtIndex(int index, T& data) {
+        if (index < 0 || index >= length) {
+            cout << "Index out of bounds" << endl;
+            return;
+        }
+        if (index == 0) {
+            addWaypointAtBeginning(data);
+        }
+        if (index == length - 1) {
+            addWaypointAtEnd(data);
+        } else {
+            Node<T>* newNode = new Node<T>(data);
+            Node<T>* temp = getWaypoint(index - 1);
+            newNode->next = temp->next;
+            newNode->prev = temp;
+            temp->next->prev = newNode;
+            temp->next = newNode;
+            length++;
+        }
+    }
+
+    void removeWaypointAtBeginning() {
+        if (length == 0) {
+            cout << "The route is empty" << endl;
+            return;
+        }
+        Node<T>* temp = head;
+        if (length == 1) {
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            head = head->next;
+        }
+        delete temp,
+        length--;
+    }
+
+    void removeWaypointAtEnd() {
+        if (length == 0) {
+            cout << "The route is empty" << endl;
+            return;
+        }
+        Node<T>* temp = head;
+        if (length == 1) {
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            Node<T>* pre = head;
+            while (temp->next) {
+                pre = temp;
+                temp = temp->next;
+            }
+            tail = pre;
+            tail->next = nullptr;
+        }
+        delete temp,
+        length--;
+    }
+
+    void removeWaypointAtIndex(int index) {
+        if (index < 0 || index >= length) {
+            cout << "Index out of bounds" << endl;
+            return;
+        }
+        if (index == 0) {
+            removeWaypointAtBeginning();
+        }
+        if (index == length - 1) {
+            removeWaypointAtEnd();
+        }
+
+        Node<T>* prev = getWaypoint(index - 1);
+        Node<T>* temp = prev->next;
+
+        prev->next = temp->next;
+        delete temp;
+        length--;
+    }
+
+    void traverseForward() {
+        if (length == 0) {
+            cout << "The route is empty" << endl;
+            return;
+        }
+        Node<T>* current = head;
+        while (current) {
+            current->print();
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void traverseBackward() {
+        if (length == 0) {
+            cout << "The route is empty" << endl;
+            return;
+        }
+        Node<T>* current = tail;
+        while (current) {
+            current->print();
+            current = current->prev;
+        }
+        cout << endl;
+    }
+
+    Node<T>* getWaypoint(int index) {
+        if (index < 0 || index >= length) {
+            cout << "Index out of bounds" << endl;
+            return nullptr;
+        }
+        Node<T>* temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    void setWaypoint(int index, T& data) {
+        Node<T>* temp = this->getWaypoint(index);
+        if (temp) {
+            temp->data = data;
+        }
+    }
+
     void print(){
 
             Node<T>* current = head;
